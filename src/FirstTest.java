e5ad3bb7351535d37586572cfeb60e2d9a1c8673
nitrobenzol <glebsarkisov@gmail.com>
2019-04-04 23:01:50 +0300

    пишем первый тест

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -37,7 +38,16 @@ public class FirstTest {
 
     @Test
     public void firstTest()
-    {
-        System.out.println("First test run");
+    {   // пропустим интро в приложении нажатием на skip
+        WebElement element_to_skip_intro = driver.findElementByXPath("//*[contains(@text,'Skip')]");
+        element_to_skip_intro.click();
+
+        // нажмем на строку поиска для начала поиска
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        element_to_init_search.click();
+
+
+        
+        // System.out.println("First test run");
     }
 }
