af6a8c0e79a7dabf29531a6fa21db4071a85a16d
nitrobenzol <glebsarkisov@gmail.com>
2019-04-05 20:02:06 +0300

    добавил метод для ожидания появления элементов

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -46,9 +49,33 @@ public class FirstTest {
         WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
         element_to_init_search.click();
 
-        WebElement element_to_enter_search_line = driver.findElementById("org.wikipedia:id/search_src_text");
+        WebElement element_to_enter_search_line = waitForElementPresentById(
+                "org.wikipedia:id/search_src_text",
+                "Cannot find search input"
+        );
+
+                //driver.findElementById("org.wikipedia:id/search_src_text");
         element_to_enter_search_line.sendKeys("Appium");
 
         // System.out.println("First test run");
+    }
+
+    // метод таймаута
+    private WebElement waitForElementPresentById(String id, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.id(id);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+
+    }
+
+    // перегрузка метода позволяет использовать таймаут по умолчанию - то есть,
+    // если указать значение, то будет использовано прописанное, а если нет, то по умолчанию
+    private WebElement waitForElementPresentById(String id, String error_message)
+    {
+        return waitForElementPresentById(id, error_message, 5);
     }
 }
