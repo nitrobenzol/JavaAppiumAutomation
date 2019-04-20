886799adb1e74cbc33a7e8ca343b5717f0554ba9
nitrobenzol <glebsarkisov@gmail.com>
2019-04-20 20:26:47 +0300

    рефакторинг: инициализация драйвера вынесена в отдельный файл

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,6 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -13,39 +10,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-
-    private AppiumDriver driver;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/g.sarkisov/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities); // передали андроид-драйверу
-        // все необходимые для запуска параметры
-
-    }
-
-    @After
-    public void tearDown()
-    {
-        driver.quit();
-    }
+public class FirstTest extends CoreTestCase {
 
     @Test
-    public void firstTest()
+    public void testSearch()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
@@ -217,7 +187,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList()
+    public void testSaveFirstArticleToMyList()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
