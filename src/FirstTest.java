b9e7a54b1ae61990f446531af55de050d8e64664
nitrobenzol <glebsarkisov@gmail.com>
2019-04-20 14:19:21 +0300

    добавлен тест для ухода приложения в бэкграунд и возврата из него

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -444,6 +444,37 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void testCheckSearchArticleInBackground()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        driver.runAppInBackground(2);
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find article after returning from background",
+                5
+        );
+    }
+
     // метод таймаута
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
     {
