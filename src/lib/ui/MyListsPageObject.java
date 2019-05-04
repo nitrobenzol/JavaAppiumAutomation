7a06ae92d8fff85a10974b2d6b82bf575520276e
nitrobenzol <glebsarkisov@gmail.com>
2019-05-04 21:05:06 +0300

    переписан тест для сохранения статьи в список и удаления затем из него

--- src/lib/ui/MyListsPageObject.java
+++ src/lib/ui/MyListsPageObject.java
@@ -1,6 +1,7 @@
 package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
+import lib.Platform;
 
 abstract public class MyListsPageObject extends MainPageObject {
 
@@ -55,7 +56,9 @@ abstract public class MyListsPageObject extends MainPageObject {
                 article_xpath,
                 "Cannot find saved article"
         );
-
+        if(Platform.getInstance().isIOS()){
+        this.clickElementToTheRightUpperCorner(article_xpath, "Cannot find saved article");
+        }
         this.waitForArticleToDisappearByTitle(article_title);
     }
 
